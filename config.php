88b93469171c332adaee6ce6301ef87b6d80b75b
root <root@vm12080024.(none)>
2013-03-01 18:00:02 +0800

    add code

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+define('APIKEY','00407a231814c0c500c16e69483e424f');
+define('Secret','7d35f14dd70445d7');
+define( "CALLBACK_URL" , 'http://gequ.sinaapp.com/douban/doubancallback.php' );//回调地址
